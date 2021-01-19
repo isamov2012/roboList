2351ff80f42c5b793932cab1ad1f1a5fc3149abf
Isam <isamov2012@hotmail.com>
2021-01-19 14:25:48 +0100

    Updates

---
+++ precache-manifest.74509b6de77caacb11366a6720f9999d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20dccaa2e389c236d4d10cb402f3b289",
+    "url": "/roboList/index.html"
+  },
+  {
+    "revision": "c9abcb58d93276ebe51b",
+    "url": "/roboList/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "723ea4d2654f6f4cd333",
+    "url": "/roboList/static/css/main.8db468e9.chunk.css"
+  },
+  {
+    "revision": "c9abcb58d93276ebe51b",
+    "url": "/roboList/static/js/2.c5eeb11c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboList/static/js/2.c5eeb11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723ea4d2654f6f4cd333",
+    "url": "/roboList/static/js/main.4eb8833b.chunk.js"
+  },
+  {
+    "revision": "f107b9832cf5fbcbf4fb",
+    "url": "/roboList/static/js/runtime-main.e4e702ea.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboList/static/media/SEGA.12a3db8b.woff"
+  }
+]);
